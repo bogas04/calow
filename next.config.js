4931feb8ce25d1eaad075d36667863bbd93b9078
Divjot Singh <divjot.singh@udaan.com>
2020-09-21 14:30:15 +0530

    chore: disable dev indicator

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ const { name } = require("./package.json");
 
 module.exports = {
   basePath: process.env.NODE_ENV === "production" ? `/${name}` : undefined,
+  devIndicators: {
+    autoPrerender: false,
+  },
 };
