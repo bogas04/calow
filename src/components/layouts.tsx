b0e0d98beeff33aa466b33eb528b5e530067deed
Divjot Singh <divjot.singh@udaan.com>
2020-09-26 17:21:36 +0530

    refactor: support non-text headings

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -4,9 +4,13 @@ export const Page = ({
   heading,
   children,
   ...props
-}: BoxProps & { heading?: string }) => (
+}: BoxProps & { heading?: React.ReactNode }) => (
   <Box py={["2", "6", "12"]} px={["4", "16", "32"]} {...props}>
-    {heading && <Heading my="6">{heading}</Heading>}
+    {heading && typeof heading === "string" ? (
+      <Heading my="6">{heading}</Heading>
+    ) : (
+      heading
+    )}
     {children}
   </Box>
 );
