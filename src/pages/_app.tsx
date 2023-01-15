b01db65070fde0d14172c251c5bff9444771dda6
Divjot Singh (bogas04) <bogas04@gmail.com>
2023-01-16 00:35:04 +0530

    fix: attempt to fix zoom issues on meal-entry page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -52,7 +52,10 @@ function App({
           <meta name="msapplication-navbutton-color" content="#48bb78" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="msapplication-starturl" content="/" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
+          />
 
           <link rel="apple-touch-icon" sizes="180x180" href={pageUrl + "icon-192-192.png"} />
           <link rel="icon" type="image/png" sizes="32x32" href={pageUrl + "icon-48-48.png"} />
