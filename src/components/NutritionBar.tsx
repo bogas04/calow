d115c03bc451486e2abc38be9386edcddbb02785
bogas04 <bogas04@gmail.com>
2025-06-16 15:48:26 +0530

    chore: round by default, who cares about 0.23kCal

--- src/components/NutritionBar.tsx
+++ src/components/NutritionBar.tsx
@@ -26,7 +26,7 @@ function NutritionBar({
   border = true,
   showLegend = !border,
   transparentBg = false,
-  round = false,
+  round = true,
 }: NutritionBarProps) {
   return (
     <div className="flex flex-col">
