545e07d5cc7a361e9b9619ed3c9dec10fea7e3f6
bogas04 <bogas04@gmail.com>
2026-01-19 15:26:56 +0530

    fix: ssr

--- src/components/useInputMode.tsx
+++ src/components/useInputMode.tsx
@@ -1,4 +1,4 @@
 export function useNumericInputMode() {
-  // on iOS use text, otherwise use numeric
-  return /iPhone|iPad|iPod/i.test(navigator.userAgent) ? "text" : "numeric";
+  // on server send numeric, on iOS use text, otherwise use numeric
+  return typeof window === "undefined" ? "numeric" : /iPhone|iPad|iPod/i.test(navigator.userAgent) ? "text" : "numeric";
 }
